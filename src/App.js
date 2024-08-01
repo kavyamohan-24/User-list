2fe9f00f640754c0d18f3491a278342bb7db4f3d
kavyamohan-24 <kavyamohan2005@gmail.com>
2024-08-01 20:44:51 +0530

    user list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Add from './components/Add';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Search from './components/Search';
 
 function App() {
   return (
-    <div>
-      <Add/>
-    </div>
+   <BrowserRouter>
+   <Routes>
+    <Route path="/" element={<Add/>}/>
+    <Route path="/search" element={<Search/>}/>
+   </Routes>
+   </BrowserRouter>
   );
 }
 
